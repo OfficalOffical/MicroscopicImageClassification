a67903faf5d44e16f136d49a2ee9abf5cfe06fc4
Sefa <mail@sefakalkan.com>
2022-05-17 21:09:44 +0300

    1.0

--- getAndEditCsv.py
+++ getAndEditCsv.py
@@ -1,15 +1,16 @@
 import cv2
 import glob
 import numpy as np
-
-datasetPath = "C:/Users/Sefa/Desktop/odevDB"
-width = 128
-height = 128
-resizeRate = (width, height)
+import pandas as pd
 
 
 
-def getImagesFromDest():
+
+
+
+def getImagesFromDest(width,height,a):
+        resizeRate = (width, height)
+        datasetPath = "C:/Users/Sefa/Desktop/odevDB"
         tempImage = []
         tempSetId = []
         images = []
@@ -18,13 +19,23 @@ def getImagesFromDest():
 
 
 
+        if (a == 0):
+                datasetPath = "C:/Users/Sefa/Desktop/tempOdevDB"
+                csvRead = pd.read_excel("tempDummyCsv.xlsx")
+        else:
+                csvRead = pd.read_excel("dummyCsv.xlsx")
+
         for x in range(len(imageNameSet)):
                 entirePath = datasetPath + imageNameSet[x] + "/*.png"
-                images.append([cv2.imread(file) for file in glob.glob(entirePath)])
+                images.append([cv2.resize(cv2.imread(file),resizeRate) for file in glob.glob(entirePath)])
+
+        images = images[0] + images[1] + images[2] + images[3] + images[4] + images[5]
+
+        return np.array(csvRead[0]),np.array(images)
 
 
 
-        cv2.imshow("A", images[0][0])
-        cv2.waitKey()
+
+
 
 
--- model.py
+++ model.py
@@ -1,8 +1,201 @@
+import keras
+from keras.applications.resnet_v2 import ResNet50V2
+from keras.applications.vgg19 import VGG19
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, Activation, MaxPooling2D, ZeroPadding2D
+from sklearn.model_selection import train_test_split
+from keras.layers import BatchNormalization
 
+from keras.layers import normalization
+import numpy as np
 
 from getAndEditCsv import getImagesFromDest
 
+width = 224
+height = 224
+#VGG, Resnet= 244
+
 
 def mainModel():
-    csv = getImagesFromDest()
-    print(csv)
+    #0 = small dataset 1 =  full
+    csv,img = getImagesFromDest(width,height,0)
+
+
+    trainX, testX, trainY, testY = train_test_split(img, csv , test_size=0.3, random_state=42)
+
+    runOwnModel(trainX,testX,trainY,testY)
+
+
+
+
+
+
+
+
+
+
+
+def createKerasResnetModel():
+
+    base_model = ResNet50V2(weights='imagenet',
+                            include_top=False,
+                            input_shape=(width, height,3))
+
+    base_model.trainable = False
+
+    model = Sequential([
+        base_model,
+
+        Flatten(),
+        Dense(6, activation="softmax"),
+
+    ])
+    return model
+
+def runResnetKerasModel(trainX,testX,trainY,testY):
+    modelKeras = createKerasResnetModel()
+
+    modelKeras.compile(loss='sparse_categorical_crossentropy', optimizer="ADAM",
+                       metrics=['sparse_categorical_accuracy'])
+
+    modelKeras.fit(trainX, trainY, epochs=20,validation_data=(testX,testY))
+
+
+
+def createKerasVGG19Model():
+    base_model = VGG19( weights='imagenet',
+                        include_top=False,
+                        input_shape=(width, height,3),
+
+                       )
+    base_model.trainable = False
+
+    model = Sequential([
+        base_model,
+
+        Flatten(),
+        Dense(6, activation="softmax"),
+
+    ])
+    return model
+
+def runKerasVGGModel(trainX,testX,trainY,testY):
+    modelKeras = createKerasVGG19Model()
+
+    modelKeras.compile(loss='sparse_categorical_crossentropy', optimizer="ADAM",
+                       metrics=['sparse_categorical_accuracy'])
+
+    modelKeras.fit(trainX, trainY, epochs=20,validation_data=(testX,testY))
+
+def createKerasAlexNetModel():
+
+    model = Sequential([
+        #1. layer
+        Conv2D(96, (11, 11), input_shape=(width,height,3),
+               padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(2,2)),
+
+        #2. layer
+        Conv2D(128, (5, 5), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        # 3. layer
+        ZeroPadding2D((1,1)),
+        Conv2D(128, (3, 3), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        # 4. layer
+        ZeroPadding2D((1, 1)),
+        Conv2D(128, (3, 3), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+
+
+        # 5. layer
+        ZeroPadding2D((1, 1)),
+        Conv2D(128, (3, 3), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        # 6. layer
+        Flatten(),
+        Dense(3072),
+        BatchNormalization(),
+        Activation('relu'),
+        Dropout(0.5),
+
+        # 7. layer
+        Dense(4096),
+        BatchNormalization(),
+        Activation('relu'),
+        Dropout(0.5),
+
+        # 8. layer
+        Dense(6),
+        BatchNormalization(),
+        Activation('softmax'),
+
+    ])
+
+    return model
+
+def runKerasAlexNetModel(trainX,testX,trainY,testY):
+    modelKeras = createKerasAlexNetModel()
+
+    modelKeras.compile(loss='sparse_categorical_crossentropy', optimizer="ADAM",
+                       metrics=['sparse_categorical_accuracy'])
+
+
+
+    modelKeras.fit(trainX, trainY, epochs=20, validation_data=(testX, testY))
+
+
+
+def createOwnModel():
+
+
+    model = Sequential([
+        Conv2D(64,(8,8), input_shape=(width, height, 3)),
+        MaxPooling2D(pool_size=(2,2)),
+        Dense(128,activation='relu'),
+
+        Conv2D(128, (8, 8), input_shape=(width, height, 3)),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dense(128, activation='relu'),
+
+        Conv2D(128, (4, 4), input_shape=(width, height, 3)),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dense(128, activation='relu'),
+
+        Conv2D(64, (4, 4), input_shape=(width, height, 3)),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dense(64, activation='relu'),
+
+
+
+
+        Flatten(),
+
+        Dense(6, activation="softmax"),
+
+    ])
+    return model
+
+def runOwnModel(trainX,testX,trainY,testY):
+    modelKeras = createOwnModel()
+
+    modelKeras.summary()
+
+    modelKeras.compile(loss='sparse_categorical_crossentropy', optimizer="ADAM",
+                       metrics=['sparse_categorical_accuracy'])
+
+    modelKeras.fit(trainX, trainY, epochs=20, validation_data=(testX, testY))
+
+
